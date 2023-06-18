ee794934c5f9bb87d51185e5937da4e5353c423f
Scott Stevenson <scott@stevenson.io>
2023-06-18 11:07:13 +0100

    Add initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use git2::Repository;
+
 fn main() {
-    println!("Hello, world!");
+    let repo = Repository::discover(".").unwrap();
+    let remotes = repo.remotes().unwrap();
+
+    if remotes.is_empty() {
+        println!("Repository has no remotes");
+    } else {
+        for name in remotes.iter() {
+            let remote = repo.find_remote(name.unwrap()).unwrap();
+            let url = remote.url().unwrap();
+
+            let url = url.strip_prefix("https://").unwrap_or(url);
+            let url = url.strip_prefix("git@").unwrap_or(url);
+            let url = url.strip_suffix(".git").unwrap_or(url);
+            let url = url.replace(':', "/");
+            let url = format!("https://{}", url);
+
+            webbrowser::open(&url).unwrap();
+        }
+    }
 }
